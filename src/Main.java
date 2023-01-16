b8dd533fc8a137d0ddb2a195e0ec2e462c326b45
Topce <425363@mail.ru>
2023-01-16 21:57:06 +0700

    Change Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        int price_ticket = 10000;
+        int price_ticket = 2500;
         int amount = 20;
+        
+        int miles;
 
-        int miles = price_ticket / amount;
+        if (amount >=20) {
+            miles = price_ticket / amount;
+        } else miles = 0;
 
         System.out.println("Количество начисленных миль: " + miles);
     }
