2608968a75fb0a5f9c07a30dc0f6b1f982a782bc
Vyacheslav Primak <topce@icloud.com>
2023-01-17 03:14:33 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        int price_ticket = 2500;
+        int priceTicket = 2500;
         int amount = 20;
         
         int miles;
 
         if (amount >=20) {
-            miles = price_ticket / amount;
-        } else miles = 0;
-
+            miles = priceTicket / amount;
+        } else {
+            miles = 0;
+        }
         System.out.println("Количество начисленных миль: " + miles);
     }
 }
